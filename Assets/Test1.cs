85992165d875b7c51a9272dabe46384a80e4e667
Yuto Michishita <y-michishita@i-enter.co.jp>
2019-04-10 12:21:53 +0900

    ScriptTestプロジェクト　初めてのコミット lesson4を見ながら作成したファイルです。

--- Assets/Test1.cs
+++ Assets/Test1.cs
@@ -1,37 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class Test1 : MonoBehaviour {
-//Bossクラス：更新されるmpに応じて文を出力する
-	public class Boss{
-		//初期mp
-		int mp=53;
-//magic関数
-		public void magic(int damage){
-			//mpからダメージが引ける場合
-			if (this.mp >= damage) {
-				//mpからダメージを引く
-				this.mp = this.mp - damage;
-				Debug.Log ("魔法攻撃をした。残りMPは" + this.mp + "。");
-				//引けなくなったら（次引いたらマイナスになる）
-			} else {
-				Debug.Log ("MPが足りないため魔法が使えない。");
-			}
-		}
+public class Boss {        
+	private int hp = 100;          // 体力
+	private int power = 25; // 攻撃力
+	private int mp = 53;
+	// 攻撃用の関数
+	public void Attack() { 
+		Debug.Log( this.power + "のダメージを与えた" );
 	}
 
+	// 防御用の関数
+	public  void Defence(int damage) { 
+		Debug.Log( damage+"のダメージを受けた" );
+		// 残りhpを減らす
+		this.hp -= damage;
+	}
 
-
-
-	// Use this for initialization
-	void Start () {
-		int i;
-		//Bossのインスタンスbossを作り、代入
-		Boss boss = new Boss ();
-		//mpを減らしてみる
-		for (i = 0; i <= 12; i++) {
-			boss.magic (5);
+	public void Magic (int m_mp){
+		this.mp = this.mp - m_mp;
+		if (this.mp >= 5) {
+			Debug.Log ("魔法攻撃をした。残りMPは" + this.mp + "。");
+		} else {
+			Debug.Log ("MPが足りないため魔法が使えない。");
 		}
+
 	}
 }
+
+public class Test1 : MonoBehaviour {
+
+	void Start () {
+		// Bossクラスの変数を宣言してインスタンスを代入
+		Boss lastboss = new Boss ();
+		int i;
+		for (i = 0; i < 12; i++) {
+			// 攻撃用の関数を呼び出す
+			lastboss.Attack ();
+			// 防御用の関数を呼び出す
+			lastboss.Defence (3);
+			//魔法攻撃の関数を呼び出す
+			lastboss.Magic (5);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	}
+
