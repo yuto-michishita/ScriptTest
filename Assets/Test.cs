e71617c01668434e131d9138fe8242fd7bde8a9a
Yuto Michishita <y-michishita@i-enter.co.jp>
2019-04-10 12:05:47 +0900

    ScriptTestプロジェクト　初めてのコミット lesson4を見ながら作成したファイルです。

---
+++ Assets/Test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int[] array = new int[5];
+		int i;
+
+		//配列arrayの初期化
+		for (i = 0; i < 5; i++) {
+			array [i] = i;
+		}
+		Debug.Log("正順");
+		//配列array出力　正順
+		for (i = 0; i < 5; i++) {
+			Debug.Log(array[i]);
+		}
+		Debug.Log("逆順");
+		//配列arrayの出力　逆順
+		for (i = 4; i >=0 ; i--) {
+			Debug.Log(array[i]);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Test1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour {
+//Bossクラス：更新されるmpに応じて文を出力する
+	public class Boss{
+		//初期mp
+		int mp=53;
+//magic関数
+		public void magic(int damage){
+			//mpからダメージが引ける場合
+			if (this.mp >= damage) {
+				//mpからダメージを引く
+				this.mp = this.mp - damage;
+				Debug.Log ("魔法攻撃をした。残りMPは" + this.mp + "。");
+				//引けなくなったら（次引いたらマイナスになる）
+			} else {
+				Debug.Log ("MPが足りないため魔法が使えない。");
+			}
+		}
+	}
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		int i;
+		//Bossのインスタンスbossを作り、代入
+		Boss boss = new Boss ();
+		//mpを減らしてみる
+		for (i = 0; i <= 12; i++) {
+			boss.magic (5);
+		}
+	}
+}
